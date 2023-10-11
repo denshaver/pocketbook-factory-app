7eb8987751e3eae858d241ce0bdd605903369ffe
Eugeshakw <eugeshakw@gmail.com>
2023-10-11 19:41:21 +0300

    uppdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import TitleStart from "./asked/titleStatrt/titleStart";
+import Header from "./header/header";
 
 
 
 
 export const App = () => {
   return (
+    <>
+    <Header/>
     <TitleStart/>
+    </>
+    
   );
 };
---
+++ src/components/header/header.jsx
@@ -0,0 +1,25 @@
+
+const Header = () => {
+
+
+    return(
+        <>
+        <div>
+            <h1>
+                PocketBook
+            </h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        </>
+    )
+};
+
+
+export default Header;
