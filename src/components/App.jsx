f4e8a3a67f908c7392a33549c4554458c5d623e4
Eugeshakw <eugeshakw@gmail.com>
2023-10-26 15:34:32 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-
 import TitleStart from './asked/titleStatrt/titleStart';
 import Header from './header/header';
-import VideoPlayer from './video/Video';
+import { AspectRatio } from '@chakra-ui/react';
 import style from './appStyle.module.scss';
 export const App = () => {
   return (
     <>
       <div className={style.container}>
         <Header />
-        <VideoPlayer/>
+        <AspectRatio maxW="1360px" className={style.video}>
+          <iframe
+            title="naruto"
+            src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+            
+          />
+        </AspectRatio>
         <TitleStart />
       </div>
     </>
