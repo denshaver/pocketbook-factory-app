83c9e2c6624dfbfce53e085f30bc9611922375ac
Eugeshakw <eugeshakw@gmail.com>
2023-11-08 13:40:17 +0200

    uppdate

--- src/components/video/myVideoPlayer.jsx
+++ src/components/video/myVideoPlayer.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { Player, ControlBar, BigPlayButton } from 'video-react';
 import 'video-react/dist/video-react.css';
 
-// import video from './animation.mp4'
+import video from './animation.mp4'
 
 
  const  MyVideoPlayer = () => {
   return (
    
 
-    <Player autoPlay className={style.aspectvideo}>
+    <Player autoPlay src={video} className={style.aspectvideo}>
       
         <BigPlayButton position="center" />
       <ControlBar autoHide={false}/>
