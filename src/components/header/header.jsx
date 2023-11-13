e3169a3f49dbbff64645103478395ba9874e6519
Eugeshakw <eugeshakw@gmail.com>
2023-11-14 00:33:46 +0200

    uppdate

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -54,6 +54,7 @@ const Header = () => {
           </li>
         </ul> */}
       </div>
+      
       <div className={style.conTextTittle}>
         <h2 className={style.titleWelcome}>
           {t('titleWelcome')}
