51e88030963a3a3659565d1c993975601376f885
Denys Vishchuk <denys.v@develops.today>
2023-11-27 14:05:43 +0200

    feat(video): 2 new videos (ru + ua)

--- src/utils/lngVideoChange.js
+++ src/utils/lngVideoChange.js
@@ -1,6 +1,8 @@
 import videoEn from '../components/video/animation-en.mp4';
 import videoEs from '../components/video/animation-es.mp4';
 import videoIt from '../components/video/animation-it.mp4';
+import videoUa from '../components/video/animation-ua.mp4';
+import videoRu from '../components/video/animation-ru.mp4';
 
 export const handleVideoLanguageChange = language => {
   switch (language) {
@@ -12,6 +14,12 @@ export const handleVideoLanguageChange = language => {
     }
     case 'it': {
       return videoIt;
+    }
+    case 'ua': {
+      return videoUa;
+    }
+    case 'ru': {
+      return videoRu;
     }
     default: {
       return videoEn;
